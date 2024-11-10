ae7d69a99040664c86fd18fd8247ebed13df771c
NugiHtl <nugihtl12@gmail.com>
2024-11-10 22:49:35 +0700

    menambah menu dan catatan

--- order.py
+++ order.py
@@ -12,3 +12,23 @@ print("=== Selamat Datang di Restoran Markez ===")
 print("Daftar menu:")
 for item, info in menu_item.items():
     print(f"- {item}: Rp{info['harga']}")
+
+print("=== Selamat Datang di Restoran Markez ===")
+print("Daftar menu:")
+for item, info in menu_item.items():
+    print(f"- {item}: Rp{info['harga']}")
+for i in range(5):
+    nama_item = input(f"\nMasukkan nama item yang ingin dipesan (pesanan ke-{i+1}, atau tekan Enter untuk selesai): ").title()
+    if not nama_item:
+        break
+    if nama_item in menu_item:
+        jumlah_input = input(f"Masukkan jumlah {nama_item}: ")
+        if jumlah_input.isdigit() and int(jumlah_input) > 0:
+            jumlah = int(jumlah_input)
+            catatan = input(f"Tambahkan catatan khusus untuk {nama_item} (tekan Enter jika tidak ada): ")
+            keranjang.append({"item": nama_item, "jumlah": jumlah, "catatan": catatan})
+            print(f"{jumlah} {nama_item} ditambahkan ke keranjang dengan catatan: '{catatan}'.")
+        else:
+            print("Jumlah harus berupa angka positif.")
+    else:
+        print("Item tidak ditemukan dalam menu.")
