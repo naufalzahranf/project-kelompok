4e1c418e2ce4cb571ca4dbcaddf0606132d014b4
felixskdeleton <naufalzahran1979@gmail.com>
2024-11-10 21:37:37 +0700

    inisialisasi menu dan minimal belanja

---
+++ order.py
@@ -0,0 +1,9 @@
+menu_item = {
+    "Nasi Goreng": {"harga": 30000},
+    "Ayam Bakar": {"harga": 40000},
+    "Teh Manis": {"harga": 10000},
+    "Kopi Hitam": {"harga": 15000}
+}
+
+MINIMUM_BELANJA = 75000
+keranjang = []
